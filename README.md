89348106653cb3e219b9fbf91c4be84a3c0e9cd7
CaoMeiYouRen <996881204@qq.com>
2020-12-17 20:32:32 +0800

    feat: 新增 页面内跳转
    fix #1

--- README.md
+++ README.md
@@ -25,10 +25,23 @@
 
 ## 项目简介
 
--   出现异常时将自动跳转到搜索引擎，可自定义处理方式。
+-   出现异常时将自动跳转到搜索引擎
     -   中文环境跳转百度，英文环境跳转 Google
+    -   可自定义搜索引擎。
 -   纯属娱乐，生产环境请勿使用，否则后果自负
 -   以前在网上看到过类似的处理方法，现在闲的无聊就把它实现和优化了一下
+
+## 核心代码
+
+```ts
+export async function funnyCatchError(cb: () => any, config: CatchConfig = {}) {
+    try {
+        await cb()
+    } catch (error) {
+        window.open(`https://www.baidu.com/s?wd=${error.message}`)
+    }
+}
+```
 
 ## 安装
 
@@ -42,30 +55,31 @@ npm i -D funny-catch-error
 import { funnyCatchError } from 'funny-catch-error'
 // 直接使用
 funnyCatchError(() => {
-	throw new Error('xxxx is undefined') // 抛出的异常将会自动跳转页面到搜索引擎，例如 `https://www.baidu.com/s?wd=xxxx is undefined`
+    throw new Error('xxxx is undefined') // 抛出的异常将会自动跳转页面到搜索引擎，例如 `https://www.baidu.com/s?wd=xxxx is undefined`
 }
+// 也支持异步函数 
+funnyCatchError(async () => {
+    throw new Error('async 异步异常处理')
+})
 // 还可以传入第二个参数作为配置项，具体说明见下文
 funnyCatchError(() => {
     throw new Error('xxxx is undefined')
 }, {
     searchEngines: 'https://cn.bing.com/search?q=[key]',
-    callback(error) {
-        console.error(error)
-        console.log('程序又TM出错了')
-    }
 })
+
 ```
 
 浏览器端也可以直接引入 `index.js` 文件，现代浏览器可以使用 `index.esm.js` 版本
 
 ```html
-<script src="https://cdn.jsdelivr.net/npm/funny-catch-error@1.0.0/dist/index.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/funny-catch-error@latest/dist/index.js"></script>
 <script>
-	const { funnyCatchError } = FunnyCatchError
+    const { funnyCatchError } = FunnyCatchError
     funnyCatchError(() => {
-		throw new Error('xxxx is undefined')
-	}
-</script> 
+        throw new Error('xxxx is undefined')
+    })
+</script>
 ```
 
 ## 配置项
@@ -73,17 +87,18 @@ funnyCatchError(() => {
 ```ts
 interface CatchConfig {
     /**
-     * 自定义搜索引擎。若配置该项将覆盖默认配置。
+     * 自定义搜索引擎。若配置该项将覆盖默认配置。[key] 会被替换为具体 errorMessage
      * 例如：https://cn.bing.com/search?q=[key]
      *
      * @type {string}
      */
     searchEngines?: string
     /**
-     * 自定义错误处理方式。若配置该项将忽略 searchEngines 配置
+     * 是否打开新的窗口。默认为中文环境 true ，英文环境 false
      *
+     * @type {boolean}
      */
-    callback?: (error: unknown) => any
+    isOpenNewWindow?: boolean
 }
 ```
 
@@ -113,4 +128,4 @@ Copyright © 2020 [CaoMeiYouRen](https://github.com/CaoMeiYouRen).<br />
 This project is [MIT](https://github.com/CaoMeiYouRen/funny-catch-error/blob/master/LICENSE) licensed.
 
 ***
-_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,42 @@
 interface CatchConfig {
     /**
-     * 自定义搜索引擎。若配置该项将覆盖默认配置。
+     * 自定义搜索引擎。若配置该项将覆盖默认配置。[key] 会被替换为具体 errorMessage
      * 例如：https://cn.bing.com/search?q=[key]
      *
      * @type {string}
      */
     searchEngines?: string
     /**
-     * 自定义错误处理方式。若配置该项将忽略 searchEngines 配置
+     * 是否打开新的窗口
      *
+     * @type {boolean}
      */
-    // eslint-disable-next-line no-unused-vars
-    callback?: (error: unknown) => any
+    isOpenNewWindow?: boolean
 }
 
-function openNewWindow(url: string) {
-    return window.open(url)
+interface SearchConfig extends CatchConfig {
+    /**
+     * 错误信息
+     *
+     * @type {string}
+     */
+    errorMessage: string
+}
+/**
+ * 跳转新的窗口或页面
+ *
+ * @author CaoMeiYouRen
+ * @date 2020-12-17
+ * @param {string} url 要跳转的路径
+ * @param {boolean} [open] 是否打开新的窗口
+ * @returns
+ */
+function openNewWindow(url: string, open?: boolean) {
+    if (open){
+        return window.open(url)
+    }
+    window.location.href = url
+    return window
 }
 
 function getLanguage() {
@@ -26,25 +47,37 @@ function getLanguage() {
     return 'en'
 }
 
-function openSearchEngines(errorMessage: string) {
+function openSearchEngines(config: SearchConfig) {
+    const { errorMessage, searchEngines, isOpenNewWindow } = config
     const language = getLanguage()
-    switch (language) {
-        case 'zh':
-            return openNewWindow(`https://www.baidu.com/s?wd=${errorMessage}`)
-        default:
-            return openNewWindow(`https://www.google.com/search?&q=${errorMessage}`)
+    // 如果定义了 isOpenNewWindow 则按 isOpenNewWindow，未定义则中文环境 open ，英文环境不 open
+    const open = typeof isOpenNewWindow === 'boolean' ? isOpenNewWindow : language === 'zh'
+    let url = ''
+    if (searchEngines){
+        url = searchEngines.replace('[key]', errorMessage)
+    } else {
+        switch (language) {
+            case 'zh':
+                url = `https://www.baidu.com/s?wd=${errorMessage}`
+                break
+            default:
+                url = `https://www.google.com/search?&q=${errorMessage}`
+                break
+        }
     }
+    return openNewWindow(url, open)
 }
 
 /**
  * 捕获异常。本函数只能工作在浏览器环境
  *
  * @author CaoMeiYouRen
- * @date 2020-12-16
+ * @date 2020-12-17
  * @export
- * @param {() => any} cb
+ * @param {() => any} cb 要执行的函数，支持异步函数
+ * @param {CatchConfig} [config={}] 配置项
  */
-export async function funnyCatchError(cb: () => any, config?: CatchConfig) {
+export async function funnyCatchError(cb: () => any, config: CatchConfig = {}) {
     try {
         await cb()
     } catch (error) {
@@ -58,17 +91,8 @@ export async function funnyCatchError(cb: () => any, config?: CatchConfig) {
             errorMessage = error
         }
         if (errorMessage){
-            if (config?.callback){
-                config.callback(error)
-                return
-            }
             console.error(error)
-            if (config?.searchEngines){
-                const url = config.searchEngines.replace('[key]', errorMessage)
-                openNewWindow(url)
-                return
-            }
-            openSearchEngines(errorMessage)
+            openSearchEngines({ ...config, errorMessage })
             return
         }
         throw error
